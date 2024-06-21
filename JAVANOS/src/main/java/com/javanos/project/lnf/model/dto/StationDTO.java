dd9de2f0973072f6fc35dbef4f989e915d6cab7f
sonchanmi <cksal5914@naver.com>
2024-06-21 16:57:35 +0900

    #32 feat: DownDTO 추가

---
+++ JAVANOS/src/main/java/com/javanos/project/lnf/model/dto/StationDTO.java
@@ -0,0 +1,62 @@
+package com.javanos.project.lnf.model.dto;
+
+public class StationDTO {
+	
+	private int staNo;
+	private int staCode;
+	private String staName;
+	private String staLine;
+	
+	public StationDTO() {
+		super();
+	}
+
+	public StationDTO(int staNo, int staCode, String staName, String staLine) {
+		super();
+		this.staNo = staNo;
+		this.staCode = staCode;
+		this.staName = staName;
+		this.staLine = staLine;
+	}
+
+	public int getStaNo() {
+		return staNo;
+	}
+
+	public void setStaNo(int staNo) {
+		this.staNo = staNo;
+	}
+
+	public int getStaCode() {
+		return staCode;
+	}
+
+	public void setStaCode(int staCode) {
+		this.staCode = staCode;
+	}
+
+	public String getStaName() {
+		return staName;
+	}
+
+	public void setStaName(String staName) {
+		this.staName = staName;
+	}
+
+	public String getStaLine() {
+		return staLine;
+	}
+
+	public void setStaLine(String staLine) {
+		this.staLine = staLine;
+	}
+
+	@Override
+	public String toString() {
+		return "StationDTO [staNo=" + staNo + ", staCode=" + staCode + ", staName=" + staName + ", staLine=" + staLine
+				+ "]";
+	}
+	
+	
+
+}
