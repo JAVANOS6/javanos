5a34ffe3adc710856d99eaa61dcabbb6f15a13b8
RACHEL-2929 <sts06154@naver.com>
2024-06-25 11:31:05 +0900

    CommunityInsertServlet 주석 수정

--- JAVANOS/src/main/java/com/javanos/project/community/controller/CommunityInsertServlet.java
+++ JAVANOS/src/main/java/com/javanos/project/community/controller/CommunityInsertServlet.java
@@ -232,70 +232,8 @@ public class CommunityInsertServlet extends HttpServlet {
 				}
 			}
 			
-			
-			
-			
-			
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		 * String communityTitle = request.getParameter("communityTitle"); String
-		 * communityBody = request.getParameter("communityBody"); int userNo =
-		 * Integer.valueOf(request.getParameter("userNo"));
-		 * 
-		 * String path = "";
-		 * 
-		 * System.out.println(communityTitle.length()==0);
-		 * 
-		 * 
-		 * //제목과 내용 둘 중에 하나만 비어도 등록이 안되게하기(둘다 작성해야 넘어갈 수 있음) if
-		 * (communityTitle.length()==0|| communityBody.length()==0) { path =
-		 * "/WEB-INF/views/common/fail.jsp"; request.setAttribute("message",
-		 * "제목이나 내용이 비어있습니다!!"); request.setAttribute("code", "emptyTitleorBody"); }
-		 * else { CommunityDTO community = new CommunityDTO();
-		 * community.setCommunityTitle(communityTitle);
-		 * community.setCommunityBody(communityBody); community.setUserNo(userNo);
-		 * 
-		 * int result = new CommunityService().insertCommunity(community);
-		 * 
-		 * 
-		 * 
-		 * if(result>0) { path = "/WEB-INF/views/common/success.jsp";
-		 * request.setAttribute("successCode", "communityInsert");
-		 * request.setAttribute("message", "등록되었습니다!! :)");
-		 * 
-		 * }else { path = "/WEB-INF/views/common/fail.jsp";
-		 * request.setAttribute("message", "게시판 작성에 실패하셨습니다.");
-		 * request.setAttribute("code", "communityInsert");
-		 * 
-		 * } }
-		 * 
-		 * request.getRequestDispatcher(path).forward(request, response);
-		 */
-
-	
 	}
 
 }
