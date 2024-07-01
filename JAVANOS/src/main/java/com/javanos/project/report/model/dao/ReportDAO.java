a5618343a62513aab3ab125b0b48daa991aaea29
infinite130 <infiniteto130@gmail.com>
2024-07-01 09:33:56 +0900

    240701

--- JAVANOS/src/main/java/com/javanos/project/report/model/dao/ReportDAO.java
+++ JAVANOS/src/main/java/com/javanos/project/report/model/dao/ReportDAO.java
@@ -1,5 +1,18 @@
 package com.javanos.project.report.model.dao;
 
-public interface ReportDAO {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.javanos.project.report.model.dto.ReportDTO;
+import com.javanos.project.user.model.dto.UserDTO;
+
+public interface ReportDAO {
+    List<ReportDTO> selectAllReports();
+    int insertReport(ReportDTO report);
+    ReportDTO selectReportByNo(int reportNo);
+    
+    // UserDTO 조회 메서드 추가
+    //UserDTO selectUserByUserId(@Param("userId") String userId);
+    UserDTO selectUserByUserId(String userId);
 }
--- JAVANOS/src/main/java/com/javanos/project/report/model/service/ReportService.java
+++ JAVANOS/src/main/java/com/javanos/project/report/model/service/ReportService.java
@@ -1,5 +1,49 @@
 package com.javanos.project.report.model.service;
 
-public class ReportService {
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import static com.javanos.project.common.mybatis.Template.getSqlSession;
+import com.javanos.project.report.model.dao.ReportDAO;
+import com.javanos.project.report.model.dto.ReportDTO;
+import com.javanos.project.user.model.dto.UserDTO;
 
+public class ReportService {
+    private ReportDAO reportDAO;
+
+    public List<ReportDTO> selectAllReports() {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        List<ReportDTO> reportList = reportDAO.selectAllReports();
+        session.close();
+        return reportList;
+    }
+
+    public int insertReport(ReportDTO report) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        int result = reportDAO.insertReport(report);
+        if (result > 0) {
+            session.commit();
+        } else {
+            session.rollback();
+        }
+        session.close();
+        return result;
+    }
+
+    public ReportDTO selectReportByNo(int reportNo) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        ReportDTO report = reportDAO.selectReportByNo(reportNo);
+        session.close();
+        return report;
+    }
+
+    public UserDTO selectUserByUserId(String userId) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        UserDTO user = reportDAO.selectUserByUserId(userId);
+        session.close();
+        return user;
+    }
 }
