8bf8dd7ef4d854f38b5f33b1a78eab4d6f3b2d4e
infinite130 <infiniteto130@gmail.com>
2024-07-02 09:36:04 +0900

    [나래] 신고 기능 완료 + 리포트 DB 수정

--- JAVANOS/src/main/java/com/javanos/project/report/model/service/ReportService.java
+++ JAVANOS/src/main/java/com/javanos/project/report/model/service/ReportService.java
@@ -3,12 +3,17 @@ package com.javanos.project.report.model.service;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import static com.javanos.project.common.mybatis.Template.getSqlSession;
+
 import com.javanos.project.report.model.dao.ReportDAO;
 import com.javanos.project.report.model.dto.ReportDTO;
 import com.javanos.project.user.model.dto.UserDTO;
 
 public class ReportService {
     private ReportDAO reportDAO;
+    
+    public ReportService() {
+        // ReportDAO 인터페이스를 직접 인스턴스화할 수 없습니다.
+    }
 
     public List<ReportDTO> selectAllReports() {
         SqlSession session = getSqlSession();
@@ -47,17 +52,53 @@ public class ReportService {
         return user;
     }
     
-    public int deleteReport(int reportNo) {
+    public boolean deleteReport(int reportNo) {
         SqlSession session = getSqlSession();
         reportDAO = session.getMapper(ReportDAO.class);
         int result = reportDAO.deleteReport(reportNo);
+        System.out.println("deleteReport result: " + result); // 디버깅 정보 출력
         if (result > 0) {
             session.commit();
+            return true;
         } else {
             session.rollback();
+            return false;
         }
-        session.close();
-        return result;
     }
     
+    public String getUserNicknameByUserNo(int userNo) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        String userNickname = reportDAO.selectUserNicknameByUserNo(userNo);
+        session.close();
+        return userNickname;
+    }
+
+    public boolean banUserByUserNo(int userNo) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        int result = reportDAO.banUserByUserNo(userNo);
+        System.out.println("banUserByUserNo result: " + result); // 디버깅 정보 출력
+        if (result > 0) {
+            session.commit();
+            return true;
+        } else {
+            session.rollback();
+            return false;
+        }
+    }
+
+    public boolean updateReportStatus(int reportNo, String reportStatus) {
+        SqlSession session = getSqlSession();
+        reportDAO = session.getMapper(ReportDAO.class);
+        int result = reportDAO.updateReportStatus(reportNo, reportStatus);
+        System.out.println("updateReportStatus result: " + result); // 디버깅 정보 출력
+        if (result > 0) {
+            session.commit();
+            return true;
+        } else {
+            session.rollback();
+            return false;
+        }
+    }
 }
